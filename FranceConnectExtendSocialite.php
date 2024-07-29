2e6956bf4967a9571640be2758476b00b931933e
zorx <oudjehih@zedx.org>
2024-07-29 15:38:46 +0100

    upgrade php-jwt

--- FranceConnectExtendSocialite.php
+++ FranceConnectExtendSocialite.php
@@ -13,8 +13,6 @@ class FranceConnectExtendSocialite
      */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
-        $socialiteWasCalled->extendSocialite(
-            'franceconnect', __NAMESPACE__.'\Provider'
-        );
+        $socialiteWasCalled->extendSocialite('franceconnect', Provider::class);
     }
 }
